ccf58061d035095100b2a907a2498d7b92975952
Jake Garcia <bballwiz4@gmail.com>
2020-03-16 21:43:38 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -21,6 +21,16 @@ class MusicLibraryController
       case user_input
       when 'list songs'
         self.list_songs
+      when 'list_artists'
+        self.list_artists
+      when 'list genres'
+        self.list_genres
+      when 'list artist'
+        self.list_songs_by_artist
+      when 'list genre'
+        self.list_songs_by_genre
+      when 'play song'
+        self.play_song
     end
   end
 
